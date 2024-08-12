9024a8673ac76f5cb254989e9082bc0e922223a9
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-08-13 00:27:18 +0300

    add constants for jwt and server

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,3 @@
-// Mongo Database
 /**
  * Database host
  * @constant
@@ -15,4 +14,23 @@ export const DB_PORT: string = process.env.DB_PORT || '27017';
  * Database name
  * @constant
  */
-export const DB_NAME: string = process.env.DB_NAME || 'files_manager';
+export const DB_NAME: string = process.env.DB_NAME || 'project_manager';
+
+/**
+ * the secret key for jwt token
+ * @constant
+ */
+export const JWT_SECRET_KEY: string = process.env.JWT_SECRET_KEY || 'strong_secret_key';
+
+/**
+ * Hone server host
+ * @constant
+ */
+export const SERVER_HOST: string = process.env.SERVER_HOST || '127.0.0.1';
+
+/**
+ * hono server port
+ * @constant
+ */
+export const SERVER_PORT: string = process.env.SERVER_PORT || '5000';
+
