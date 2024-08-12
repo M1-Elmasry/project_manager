d30ca04960301bf23e0ec143ba69f3ffbf84b599
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-08-12 05:40:38 +0300

    add constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,19 @@
+// Mongo Database
+/**
+ * Database host
+ * @constant
+ */
+export const DB_HOST: string = process.env.DB_HOST || '127.0.0.1';
+
+/**
+ * Database port
+ * @constant
+ */
+export const DB_PORT: string = process.env.DB_PORT || '27017';
+
+/**
+ * Database name
+ * @constant
+ */
+export const DB_NAME: string = process.env.DB_NAME || 'files_manager';
+
