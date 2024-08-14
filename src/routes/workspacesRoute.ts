a676cc1cec1cacd7dfd876e5e0260e0b1a228095
Fadi Asaad <firon1222@gmail.com>
2024-08-15 02:45:28 +0300

    add: workspaces endpoints

---
+++ src/routes/workspacesRoute.ts
@@ -0,0 +1,62 @@
+import { Hono } from 'hono';
+import WorkspacesController from '../controllers/workspacesController';
+import { verifyToken } from '../middlewares/authMiddelwares';
+import { WorkspaceGuard } from '../middlewares/workspacesMiddlewares';
+
+const route = new Hono();
+
+// get all workspaces where the user is a member
+route.get('/', verifyToken, WorkspacesController.getAllJoinedWorkspaces);
+
+route.post('/', verifyToken, WorkspacesController.createWorkspace);
+
+route.get(
+  '/:workspaceId',
+  verifyToken,
+  WorkspaceGuard(),
+  WorkspacesController.getWorkspace,
+);
+
+route.delete(
+  '/:workspaceId',
+  verifyToken,
+  WorkspaceGuard({ onlyOwner: true }),
+  WorkspacesController.deleteWorkspace,
+);
+
+route.put(
+  '/:workspaceId',
+  verifyToken,
+  WorkspaceGuard({ onlyOwner: true }),
+  WorkspacesController.updateWorkspace,
+);
+
+route.get(
+  '/:workspaceId/members',
+  verifyToken,
+  WorkspaceGuard(),
+  WorkspacesController.getWorkspaceMembers,
+);
+
+route.post(
+  '/:workspaceId/members',
+  verifyToken,
+  WorkspaceGuard({ onlyOwner: true }),
+  WorkspacesController.addMembers,
+);
+
+route.delete(
+  '/:workspaceId/members',
+  verifyToken,
+  WorkspaceGuard({ onlyOwner: true }),
+  WorkspacesController.deleteMembers,
+);
+
+route.get(
+  '/:workspaceId/projects',
+  verifyToken,
+  WorkspaceGuard(),
+  WorkspacesController.getWorkspaceProjects,
+);
+
+export default route;
