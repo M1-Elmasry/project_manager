e5a45f8c3a490e7567e718d5b80df0b86095032b
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-08-19 01:48:31 +0300

    add projects, notes, questions and replies collections into database

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,6 +3,10 @@ import { DB_HOST, DB_PORT, DB_NAME } from './constants';
 import type { Db, Collection } from 'mongodb';
 import type { UserDocument } from '../types/auth';
 import type { Workspace } from '../types/workspaces';
+import type { ProjectDocument } from '../types/projects';
+import type { NoteDocument } from '../types/projects';
+import type { QuestionDocument } from '../types/projects';
+import type { ReplyDocument } from '../types/projects';
 
 class DBClient {
   readonly host: string;
@@ -13,6 +17,10 @@ class DBClient {
   public db: Db | null = null;
   public users: Collection<UserDocument> | null = null;
   public workspaces: Collection<Workspace> | null = null;
+  public projects: Collection<ProjectDocument> | null = null;
+  public notes: Collection<NoteDocument> | null = null;
+  public questions: Collection<QuestionDocument> | null = null;
+  public replies: Collection<ReplyDocument> | null = null;
 
   constructor(host?: string, port?: string, databaseName?: string) {
     this.host = host || DB_HOST;
@@ -25,6 +33,10 @@ class DBClient {
         this.db = this.client.db(this.databaseName);
         this.users = this.db.collection('users');
         this.workspaces = this.db.collection('workspaces');
+        this.projects = this.db.collection('projects');
+        this.notes = this.db.collection('notes');
+        this.questions = this.db.collection('questions');
+        this.replies = this.db.collection('replies');
         console.log(`db connected on ${this.host}:${this.port}`);
       })
       .catch((error) => {
