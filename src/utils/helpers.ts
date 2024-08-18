5359b031033a416a6a1e9a3bbae396c204809037
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-08-19 01:54:57 +0300

    add helper function for project controllers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,5 @@
 import { ObjectId } from 'mongodb';
+import dbClient from '../utils/db';
 
 /**
  * Safely validates if an object id is valid or not without throwing an error
@@ -10,3 +11,21 @@ export const isValidObjectId = (id: string): boolean => {
     return false;
   }
 };
+
+export async function deleteProjectsWithRelatedEntities(projectIds: ObjectId[]) {
+  const projects = await dbClient.projects?.find({
+    _id: { $in: projectIds },
+  }).toArray();
+
+  if (!projects) {
+    throw new Error('no project found');
+  }
+
+  projects.map(async (project) => {
+  //await dbClient.tasks?.deleteMany({ _id: { $in: project.tasks } });
+  await dbClient.notes?.deleteMany({ _id: { $in: project.notes } });
+  await dbClient.questions?.deleteMany({ _id: { $in: project.questions } });
+  //await dbClient.replies?.deleteMany({ _id: { $in: project.replies } });
+  await dbClient.projects?.deleteOne({ _id: project._id });
+  })
+}
