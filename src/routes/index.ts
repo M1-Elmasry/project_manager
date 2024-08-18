cf8265f7c9105d0686d54c0b3d32b72433a843b2
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-08-19 01:43:10 +0300

    add base route for projects endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Hono } from 'hono';
 
 import AuthRoute from './authRoutes';
 import WorkspacesRoute from './workspacesRoute';
+import ProjectsRoute from './projectsRoutes';
 
 const app = new Hono();
 
 app.route('/auth', AuthRoute);
 app.route('/workspaces', WorkspacesRoute);
+app.route('workspaces/:workspaceId/projects',ProjectsRoute);
 
 export default app;
