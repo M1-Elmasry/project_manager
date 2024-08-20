d56e4388f8bd95031960f501a1d12590c735f281
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-08-20 19:00:54 +0300

    add base route for questions and notes endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,15 @@ import { Hono } from 'hono';
 import AuthRoute from './authRoutes';
 import WorkspacesRoute from './workspacesRoute';
 import ProjectsRoute from './projectsRoutes';
+import QuestionsRoute from './questionesRoutes';
+import NotesRoute from './notesRoutes';
 
 const app = new Hono();
 
 app.route('/auth', AuthRoute);
 app.route('/workspaces', WorkspacesRoute);
-app.route('workspaces/:workspaceId/projects',ProjectsRoute);
+app.route('workspaces/:workspaceId/projects', ProjectsRoute);
+app.route('workspaces/:workspaceId/projects/:projectId', QuestionsRoute);
+app.route('workspaces/:workspaceId/projects/:projectId', NotesRoute);
 
 export default app;
