dfce99a7bf5e281bdef261439121a7b82ee4dc23
Fadi Asaad <firon1222@gmail.com>
2024-08-25 22:13:02 +0300

    update: routes structure to use less code

--- src/routes/questionesRoutes.ts
+++ src/routes/questionesRoutes.ts
@@ -1,42 +1,21 @@
 import { Hono } from 'hono';
-import { WorkspaceGuard } from '../middlewares/workspacesMiddlewares';
-import { ProjectGuard } from '../middlewares/projectsMiddlewares';
 import QuestionsController from '../controllers/questionsControllers';
-import { AuthGuard } from '../middlewares/authMiddelwares';
 import { QuestionGuard } from '../middlewares/questionsMiddlewares';
 
 const app = new Hono();
 
-app.post(
-  '/',
-  AuthGuard,
-  WorkspaceGuard(),
-  ProjectGuard(),
-  QuestionsController.createQuestion,
-);
+app.post('/', QuestionsController.createQuestion);
 
-app.get(
-  '/',
-  AuthGuard,
-  WorkspaceGuard(),
-  ProjectGuard(),
-  QuestionsController.getAllQuestion,
-);
+app.get('/', QuestionsController.getAllQuestion);
 
 app.put(
   '/:questionId',
-  AuthGuard,
-  WorkspaceGuard(),
-  ProjectGuard(),
   QuestionGuard({ permissionMode: 'Author' }),
   QuestionsController.updateQuestion,
 );
 
 app.delete(
   '/:questionId',
-  AuthGuard,
-  WorkspaceGuard(),
-  ProjectGuard(),
   QuestionGuard({ permissionMode: 'Owner&Author' }),
   QuestionsController.deleteQuestion,
 );
--- src/routes/repliesRoutes.ts
+++ src/routes/repliesRoutes.ts
@@ -1,38 +1,15 @@
 import { Hono } from 'hono';
-import { WorkspaceGuard } from '../middlewares/workspacesMiddlewares';
-import { ProjectGuard } from '../middlewares/projectsMiddlewares';
-import { AuthGuard } from '../middlewares/authMiddelwares';
-import { QuestionGuard } from '../middlewares/questionsMiddlewares';
 import { ReplyGuard } from '../middlewares/repliesMiddlewares';
 import RepliesController from '../controllers/repliesControllers';
 
 const app = new Hono();
 
-app.post(
-  '/',
-  AuthGuard,
-  WorkspaceGuard(),
-  ProjectGuard(),
-  QuestionGuard(),
-  RepliesController.createReply,
-);
+app.post('/', RepliesController.createReply);
 
-app.put(
-  '/:replyId',
-  AuthGuard,
-  WorkspaceGuard(),
-  ProjectGuard(),
-  QuestionGuard(),
-  ReplyGuard(),
-  RepliesController.updateReply,
-);
+app.put('/:replyId', ReplyGuard(), RepliesController.updateReply);
 
 app.delete(
   '/:replyId',
-  AuthGuard,
-  WorkspaceGuard(),
-  ProjectGuard(),
-  QuestionGuard(),
   ReplyGuard({ allowAdmins: true }),
   RepliesController.deleteReply,
 );
