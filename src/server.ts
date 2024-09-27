5d0c8fc5ab048035a0dbcbc79ab50159cb894c4a
Fadi Asaad <firon1222@gmail.com>
2024-09-27 07:40:01 +0300

    rewrite the server file to use the new db api

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,25 @@
 import { serve } from '@hono/node-server';
-import { SERVER_HOST, SERVER_PORT } from './utils/constants';
+import {
+  SERVER_HOST,
+  SERVER_PORT,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+} from '@utils/constants';
+import db from '@utils/db';
 import app from './app';
 
+(async () => {
+  await db.connect();
 
+  console.log(`Database: Connected: ${DB_HOST}:${DB_PORT}/${DB_NAME}`);
 
-serve({
-  fetch: app.fetch,
-  hostname: SERVER_HOST,
-  port: parseInt(SERVER_PORT, 10),
-}).once('listening', () => {
-  console.log(`server running on ${SERVER_HOST}:${SERVER_PORT}`);
-  console.log(`Swagger docs available at http://localhost:${SERVER_PORT}/docs`);
-});
+  serve({
+    fetch: app.fetch,
+    hostname: SERVER_HOST,
+    port: parseInt(SERVER_PORT, 10),
+  }).once('listening', () => {
+    console.log(`Server: Running: ${SERVER_HOST}:${SERVER_PORT}`);
+    console.log(`Swagger docs: /docs`);
+  });
+})();
